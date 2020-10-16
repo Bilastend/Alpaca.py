e1cdc1431c722715c90dd9dda5d023e9afc80d71
Bilal Bucak <bilal.bucak@smail.inf.h-brs.de>
2020-10-16 05:22:14 +0200

    Added Doggos

--- cogs/cmds/user.py
+++ cogs/cmds/user.py
@@ -4,6 +4,10 @@ from util import jsonParser
 import random
 import keys
 
+
+def to_lower(arg):
+    return arg.lower()
+
 class User(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
@@ -24,6 +28,54 @@ class User(commands.Cog):
         embed = discord.Embed().set_image(url=url)
         embed.add_field(name="HEY!",value="{0} du hast eine Umarmung von {1} bekommen!".format(arg,ctx.author.nick))
         await ctx.send(embed=embed)
+
+    @commands.command(name="dog", aliases=["doggo"])
+    async def dog(self,ctx, *args: to_lower):
+        """Doggo pictures!
+        \'!dog\' for a random dog
+        \'!dog [breed]\' for a picture of a specific breed
+        \'!dog list\' for a list of all breeds
+        \'!dog list [breed]\' for a list of all sub-breed (written bold in \'list\')
+        """
+        if len(args) == 0:
+            url = jsonParser.getJson("https://dog.ceo/api/breeds/image/random").get("message")
+            breed = url.split("/")[4]
+            embed = discord.Embed(title=breed.title()).set_image(url=url)
+            return await ctx.send(embed=embed)
+        if len(args) == 1:
+            if not args[0] == "list":
+                jsonO = jsonParser.getJson("https://dog.ceo/api/breed/{}/images".format(args[0]))
+                if jsonO.get("status") == "error":
+                    return await ctx.send("Breed not found")
+                doggos = jsonO.get("message")
+                url = random.choice(doggos)
+                embed = discord.Embed(title=args[0]).set_image(url=url)
+                return await ctx.send(embed=embed)
+            else:
+                doggoList = jsonParser.getJson("https://dog.ceo/api/breeds/list/all").get("message")
+                embed = discord.Embed(title="All the good boys and girls",colour=0x3311AA)
+                breeds = " "
+                for doggoBreed, subBreed in doggoList.items():
+                    if(len(subBreed) > 0):
+                        breeds += "**{}** ".format(doggoBreed.title())
+                    else:
+                        breeds += "{} ".format(doggoBreed.title())
+                embed.add_field(name="\u200b",value=breeds)
+                return await ctx.send(embed=embed)
+        if len(args) == 2:
+            response = jsonParser.getJson("https://dog.ceo/api/breed/{}/list".format(args[1]))
+            if response.get("status") == "error":
+                    return await ctx.send("Breed not found")
+            subBreedList = response.get("message")
+            if len(subBreedList) <= 0:
+                return await ctx.send("No sub-breed for {}".format(args[1].title()))
+            embed = discord.Embed(colour=0x3311AA)
+            subBreedString = " "
+            for subBreed in subBreedList:
+                subBreedString += "{} ".format(subBreed.title())
+            embed.add_field(name="Sub-Breed: {}".format(args[1].title()),value=subBreedString)
+            return await ctx.send(embed=embed)
+                
 
 def setup(bot):
    bot.add_cog(User(bot))
